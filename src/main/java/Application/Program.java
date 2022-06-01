25fe5a20c4229d233e6e2fcd6a36d7709828d11c
Adriano Bianchi Crosoletto <adrianobianchi@outlook.com>
2022-06-01 00:10:30 -0300

    Transções no banco de dados

--- src/main/java/Application/Program.java
+++ src/main/java/Application/Program.java
@@ -12,24 +12,41 @@ public class Program {
     public static void main(String[] args) {
 
         Connection conn = null;
-        PreparedStatement st =null;
+        Statement st =null;
 
         try {
             conn = DB.getConnection();
-            //Deletando registro do banco
-            st = conn.prepareStatement(
-                "DELETE FROM department "
-                + "WHERE "
-                + "Id = ?");
 
-            st.setInt(1,6);
+            //Não confirmando as operações automaticamente, aguardando o programador cofirmar
+            conn.setAutoCommit(false);
 
+            st = conn.createStatement();
 
-            int rowsAffected = st.executeUpdate();
-            System.out.println("DELET realizado, " + rowsAffected + " linhas");
+            int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+
+            //Interrompendo o update e criando erro executando somente o comando 1
+//            int x = 1;
+//            if(x < 2){
+//                throw new SQLException("Erro falso!");
+//            }
+
+            int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+
+            //confirma a transação
+            conn.commit();
+
+            System.out.println("Linha 1" + rows1);
+            System.out.println("Linha 2" + rows2);
 
         }catch (SQLException e){
-            throw new DBIntegrityException(e.getMessage());
+            //Volta a transação caso tenha ocorrido erro
+            try {
+                conn.rollback();
+                throw  new DBException("Transação não foi concluida! " + e.getMessage());
+            } catch (SQLException ex) {
+                //Tentou voltar e ocorreu erro
+                throw new DBException("Erro ao voltar o banco! " + e.getMessage());
+            }
         }
         finally {
             DB.closeStatement(st);
