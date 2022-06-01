d66168d75811cc4547d8abd489d5670f913999d7
Adriano Bianchi Crosoletto <adrianobianchi@outlook.com>
2022-05-31 23:21:33 -0300

    Inserindo dados e recupendando o ID inserido

--- src/main/java/Application/Program.java
+++ src/main/java/Application/Program.java
@@ -21,18 +21,30 @@ public class Program {
                     "INSERT INTO seller "
                     + "(Name,Email,BirthDate,BaseSalary, DepartmentID)"
                     + "VALUES "
-                    +"(?,?,?,?,?)");
+                    +"(?,?,?,?,?)",
+                    Statement.RETURN_GENERATED_KEYS);
             //Alerando os ? pelos valor
-            st.setString(1,"Maria Julia");
-            st.setString(2,"maria@gmail.com");
-            st.setDate(3,new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-            st.setDouble(4,3500.0);
-            st.setInt(5,4);
+            st.setString(1,"Joao Carlos");
+            st.setString(2,"joao.carlos@hotmail.com");
+            st.setDate(3,new java.sql.Date(sdf.parse("02/10/1999").getTime()));
+            st.setDouble(4,6500.0);
+            st.setInt(5,1);
 
             //Para saber quantas linhas foram alteras
            int rowsAffected = st.executeUpdate();
 
-            System.out.println("Update! - Foram alterados: " + rowsAffected + " linhas");
+            //System.out.println("Update! - Foram alterados: " + rowsAffected + " linhas");
+
+            //tratamento para trazer o codigo do registro inserido
+            if(rowsAffected > 0){
+                ResultSet rs = st.getGeneratedKeys();
+                while (rs.next()){
+                    int id = rs.getInt(1);
+                    System.out.println("UPDATE! Id: " + id);
+                }
+            }else {
+                System.out.println("UPDATE não realizado, não houve linhas acrescentadas.");
+            }
 
         }catch (SQLException e){
             e.printStackTrace();
