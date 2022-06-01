1284526236697566fede0349e90c65e3e981541d
Adriano Bianchi Crosoletto <adrianobianchi@outlook.com>
2022-05-31 23:38:38 -0300

    Atualizar registro do banco

--- src/main/java/Application/Program.java
+++ src/main/java/Application/Program.java
@@ -9,60 +9,34 @@ import java.text.SimpleDateFormat;
 
 public class Program {
     public static void main(String[] args) {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        //Criando a  conexao
+
         Connection conn = null;
-        PreparedStatement st = null;
+        PreparedStatement st =null;
 
         try {
             conn = DB.getConnection();
-            /*//Comando SQL
+            //Atualizar registro do banco
             st = conn.prepareStatement(
-                    "INSERT INTO seller "
-                    + "(Name,Email,BirthDate,BaseSalary, DepartmentID)"
-                    + "VALUES "
-                    +"(?,?,?,?,?)",
-                    Statement.RETURN_GENERATED_KEYS);
-            //Alerando os ? pelos valor
-            st.setString(1,"Joao Carlos");
-            st.setString(2,"joao.carlos@hotmail.com");
-            st.setDate(3,new java.sql.Date(sdf.parse("02/10/1999").getTime()));
-            st.setDouble(4,6500.0);
-            st.setInt(5,1);
-            */
+                    "UPDATE seller "
+                    + "SET BaseSalary = BaseSalary + ? "
+                    + "WHERE "
+                    + "(DepartmentID = ?)");
 
-            //Inserindo dois registros na tabela
-            st = conn.prepareStatement("INSERT INTO department (Name) values ('Tecnologia'),('Desenvolvimento')",
-                    Statement.RETURN_GENERATED_KEYS);
+            st.setDouble(1,200.0);
+            st.setInt(2,1);
 
-            //Para saber quantas linhas foram alteras
-           int rowsAffected = st.executeUpdate();
-
-            //System.out.println("Update! - Foram alterados: " + rowsAffected + " linhas");
-
-            //tratamento para trazer o codigo do registro inserido
-            if(rowsAffected > 0){
-                ResultSet rs = st.getGeneratedKeys();
-                while (rs.next()){
-                    int id = rs.getInt(1);
-                    System.out.println("UPDATE! Id: " + id);
-                }
-            }else {
-                System.out.println("UPDATE não realizado, não houve linhas acrescentadas.");
-            }
+            int rowsAffected = st.executeUpdate();
+            System.out.println("UPDATE realizado, " + rowsAffected + " linhas");
 
         }catch (SQLException e){
             e.printStackTrace();
-
         }
-//        catch (ParseException e){
-//            e.printStackTrace();
-//        }
-        //Fechando a conexao
         finally {
             DB.closeStatement(st);
             DB.closeConnection();
+
         }
+
 
     }
 }
