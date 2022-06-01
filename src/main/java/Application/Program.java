9ec25f9fbbd5fdbb40a58ad8691702f3bd0af96f
Adriano Bianchi Crosoletto <adrianobianchi@outlook.com>
2022-05-31 22:53:12 -0300

    Percorrendo a tabela e trazendo o ID e Name

--- src/main/java/Application/Program.java
+++ src/main/java/Application/Program.java
@@ -3,10 +3,40 @@ package Application;
 import db.DB;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
     public static void main(String[] args) {
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+        //Criando a  conexao
+        Connection conn = null;
+        //Preparar consulta SQL
+        Statement st = null;
+        //Traz o resultado
+        ResultSet rs = null;
+
+        try{
+            //conectando ao banco
+            conn = DB.getConnection();
+
+            st = conn.createStatement();
+            //Consuta ao banco
+            rs = st.executeQuery("select * from department");
+
+            //Percorrendo a tabela e trazendo o ID e Name
+            while (rs.next()){
+                System.out.println(rs.getInt("Id") + " - " + rs.getString("Name"));
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+
+
+
+
+
     }
 }
